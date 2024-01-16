ee5e552dad1e296764ee2f0763afa83363537efd
ShengRong Liao <shengrong.liao@prudential.com.sg>
2024-01-16 15:25:23 +0800

    大小写

--- src/pages/components/Header/Header.tsx
+++ src/pages/components/Header/Header.tsx
@@ -1,15 +1,41 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./Header.less";
+import useMyNav from "../../../router/nav";
+const list = [
+  {
+    name: "首页",
+    path: "/home",
+  },
+  {
+    name: "博客列表",
+    path: "/blog-list",
+  },
+  {
+    name: "技能标签",
+    path: "",
+  },
+  {
+    name: "简介",
+    path: "",
+  },
+];
 function Header() {
+  const { go } = useMyNav();
+  const goTo = (path) => {
+    if (!path) alert("暂未开放");
+    go(path);
+  };
+
   return (
     <div className="header">
       <div className="title">BLOG 荣</div>
       <div className="headerNav">
-        <p>首页</p>
-        <p>博客列表</p>
-        <p>技能标签</p>
-        <p>简介</p>
+        {list.map((item) => (
+          <p key={item.name} onClick={() => goTo(item.path)}>
+            {item.name}
+          </p>
+        ))}
       </div>
     </div>
   );
